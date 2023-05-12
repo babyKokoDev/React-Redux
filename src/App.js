eb190e2850d74a88abea4665fb829626494af186
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-12 22:33:13 +0100

    creating my actions

--- src/App.js
+++ src/App.js
@@ -8,16 +8,33 @@ import cartItems from "./cart-items";
 import {createStore} from "redux"
 
 const initialStore = {
-  count : 10,
+  count : 0,
   name : "Elijah"
 }
 
 const reducer = (state, action) => {
   console.log({state, action})
+  if (action.type === "DECREASE"){
+     return {...state, count: state.count - 1}
+  }
+  if (action.type === "INCREASE"){
+     return {...state, count: state.count + 1}
+  }
+  if (action.type === "RESET"){
+     return {...state, count: 0}
+  }
+  if (action.type === "CHANGE_NAME"){
+     return {...state, name: "Adebayonle"}
+  }
   return state
 }
 
 const store = createStore(reducer, initialStore)
+store.dispatch({type:"DECREASE"})
+store.dispatch({type:"INCREASE"})
+store.dispatch({type:"RESET"})
+store.dispatch({type:"INCREASE"})
+store.dispatch({type:"CHANGE_NAME"})
 console.log(store.getState())
 
 function App() {
