c2afb7d7b526f884f3247b0bfef41feb3d186343
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-13 22:38:25 +0100

    dispatch props

--- src/reducer.js
+++ src/reducer.js
@@ -2,22 +2,47 @@ import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./action";
 
 const reducer = (state, action) => {
 
-    if (action.type === CLEAR_CART){
-        return {...state, cart : []}
+    if (action.type === CLEAR_CART) {
+        return { ...state, cart: [] }
     }
-    if (action.type === DECREASE){
-        console.log("you decreased an amount");
+    if (action.type === DECREASE) {
+        const { cart } = state
+
+        if (action.payload.amount === 1) {
+            const itemLeft = cart.filter((item) => item.id !== action.payload.id)
+            return { ...state, cart: itemLeft }
+        }
+        else {
+            let newCart = cart.map((cart) => {
+                if (cart.id === action.payload.id) {
+
+                    return { ...cart, amount: cart.amount - 1 }
+
+                }
+                return cart
+            })
+            return { ...state, cart: newCart }
+
+        }
+
     }
-    if (action.type === INCREASE){
-        console.log("you increased an amount");
+    if (action.type === INCREASE) {
+        const { cart } = state
+        let newCart = cart.map((cart) => {
+            if (cart.id === action.payload.id) {
+                return { ...cart, amount: cart.amount + 1 }
+            }
+            return cart
+        })
+        return { ...state, cart: newCart }
     }
-    if (action.type === REMOVE){
-        const {cart} = state
-        const itemLeft = cart.filter((item)=>item.id !== action.payload.id)
-        return {...state, cart : itemLeft}
+    if (action.type === REMOVE) {
+        const { cart } = state
+        const itemLeft = cart.filter((item) => item.id !== action.payload.id)
+        return { ...state, cart: itemLeft }
     }
     return state
 
-  }
+}
 
-  export default reducer
+export default reducer
